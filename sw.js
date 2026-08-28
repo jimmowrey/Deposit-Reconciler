07518e6366df1bea24dab0db69c2c2fa390977b1
jimmowrey <james.mowrey1@gmail.com>
2026-08-27 20:58:00 -0700

    v1.10: refresh PWA cache for deduction display

--- sw.js
+++ sw.js
@@ -1,4 +1,17 @@
-const CACHE='deposit-reconciler-cache-app-v1.9';
+const CACHE='deposit-reconciler-cache-app-v1.10';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
